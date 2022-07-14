def66a35409e32f984d2f543e571c52039734be6
feiniao <664836141@qq.com>
2022-07-15 00:09:36 +0800

    去除弹框

--- src/main/java/com/star/easydoc/listener/AppActivationListener.java
+++ src/main/java/com/star/easydoc/listener/AppActivationListener.java
@@ -1,20 +1,7 @@
 package com.star.easydoc.listener;
 
-import java.awt.*;
-import java.net.URI;
-
-import com.intellij.icons.AllIcons.General;
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationAction;
-import com.intellij.notification.NotificationDisplayType;
-import com.intellij.notification.NotificationGroup;
-import com.intellij.notification.NotificationListener;
-import com.intellij.notification.NotificationType;
-import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationActivationListener;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.wm.IdeFrame;
-import com.star.easydoc.view.inner.SupportView;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -24,7 +11,6 @@ import org.jetbrains.annotations.NotNull;
  * @date 2022/03/13
  */
 public class AppActivationListener implements ApplicationActivationListener {
-    private static final Logger LOGGER = Logger.getInstance(AppActivationListener.class);
 
     /** 上一次通知时间 */
     private volatile long lastNoticeTime = 0L;
@@ -36,37 +22,6 @@ public class AppActivationListener implements ApplicationActivationListener {
         if (System.currentTimeMillis() - lastNoticeTime < INTERVAL) {
             return;
         }
-        NotificationGroup group = new NotificationGroup("Easy Javadoc", NotificationDisplayType.BALLOON, true, null,
-            General.AddJdk);
-        Notification notification = group.createNotification(
-            "支持EasyJavadoc", "如果这款小而精的插件为您节约了不少时间，请支持一下开发者！",
-            NotificationType.INFORMATION, NotificationListener.URL_OPENING_LISTENER);
-
-        // 去点star
-        notification.addAction(new NotificationAction("✨ 去点star") {
-            @Override
-            public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
-                try {
-                    Desktop dp = Desktop.getDesktop();
-                    if (dp.isSupported(Desktop.Action.BROWSE)) {
-                        dp.browse(URI.create("https://github.com/starcwang/easy_javadoc"));
-                    }
-                } catch (Exception ex) {
-                    LOGGER.error("打开链接失败:https://github.com/starcwang/easy_javadoc", ex);
-                }
-            }
-        });
-
-        // 支付
-        notification.addAction(new NotificationAction("☕ 请喝咖啡") {
-            @Override
-            public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
-                SupportView supportView = new SupportView();
-                supportView.show();
-            }
-        });
-
-        notification.notify(null);
         lastNoticeTime = System.currentTimeMillis();
     }
 
