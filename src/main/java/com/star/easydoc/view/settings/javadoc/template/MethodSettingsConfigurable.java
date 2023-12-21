ffba5c0307f9dcae6e3bcf236092ea5c278d770f
HRNeht <154497083+HRNeht@users.noreply.github.com>
2023-12-21 15:56:57 +0800

    Update MethodSettingsConfigurable.java

--- src/main/java/com/star/easydoc/view/settings/javadoc/template/MethodSettingsConfigurable.java
+++ src/main/java/com/star/easydoc/view/settings/javadoc/template/MethodSettingsConfigurable.java
@@ -1,58 +1,49 @@
-package com.star.easydoc.view.settings.javadoc.template;
-
-import java.util.Objects;
-import java.util.TreeMap;
-
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.options.ConfigurationException;
-import com.star.easydoc.config.EasyDocConfig;
-import com.star.easydoc.config.EasyDocConfig.TemplateConfig;
-import com.star.easydoc.config.EasyDocConfigComponent;
-import org.apache.commons.lang3.BooleanUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.Nls.Capitalization;
-
-/**
- * @author <a href="mailto:wangchao.star@gmail.com">wangchao</a>
- * @version 1.0.0
- * @since 2019-11-10 17:35:00
- */
-public class MethodSettingsConfigurable extends AbstractTemplateConfigurable<MethodSettingsView> {
-    private EasyDocConfig config = ServiceManager.getService(EasyDocConfigComponent.class).getState();
-    private MethodSettingsView view = new MethodSettingsView(config);
-
-    @Nls(capitalization = Capitalization.Title)
-    @Override
+  @Override
     public String getDisplayName() {
         return "EasyDocMethodTemplate";
     }
 
+    // 获取配置展示的视图
     @Override
     public MethodSettingsView getView() {
         return view;
     }
 
+    // 检查配置是否被修改
     @Override
     public boolean isModified() {
         TemplateConfig templateConfig = config.getMethodTemplateConfig();
+
+        // 检查是否选择了默认模板的修改
         if (!Objects.equals(templateConfig.getIsDefault(), view.isDefault())) {
             return true;
         }
+
+        // 检查自定义模板的修改
         if (!Objects.equals(templateConfig.getTemplate(), view.getTemplate())) {
             return true;
         }
+
         return false;
     }
 
+    // 应用配置
     @Override
     public void apply() throws ConfigurationException {
         TemplateConfig templateConfig = config.getMethodTemplateConfig();
+
+        // 更新是否选择默认模板的配置
         templateConfig.setIsDefault(view.isDefault());
+
+        // 更新自定义模板的配置
         templateConfig.setTemplate(view.getTemplate());
+
+        // 初始化自定义模板的 TreeMap
         if (templateConfig.getCustomMap() == null) {
             templateConfig.setCustomMap(new TreeMap<>());
         }
+
+        // 检查自定义模板的正确性
         if (!view.isDefault()) {
             if (StringUtils.isBlank(view.getTemplate())) {
                 throw new ConfigurationException("使用自定义模板，模板不能为空");
@@ -64,14 +55,19 @@ public class MethodSettingsConfigurable extends AbstractTemplateConfigurable<Met
         }
     }
 
+    // 重置配置
     @Override
     public void reset() {
         TemplateConfig templateConfig = config.getMethodTemplateConfig();
+
+        // 设置是否选择默认模板
         if (BooleanUtils.isTrue(templateConfig.getIsDefault())) {
             view.setDefault(true);
         } else {
             view.setDefault(false);
         }
+
+        // 设置自定义模板内容
         view.setTemplate(templateConfig.getTemplate());
     }
 }
